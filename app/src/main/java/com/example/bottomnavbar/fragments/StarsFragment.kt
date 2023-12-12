95afab82ae31826265354dc4b01df078c7102067
amarjane <amarjaneelmahdi03@gmail.com>
2023-12-12 21:36:53 +0100

    add stars fragment

--- app/src/main/java/com/example/bottomnavbar/fragments/StarsFragment.kt
+++ app/src/main/java/com/example/bottomnavbar/fragments/StarsFragment.kt
@@ -1,11 +1,15 @@
 package com.example.bottomnavbar.fragments
 
+import android.animation.AnimatorInflater
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.example.bottomnavbar.R
+
 
 /**
  * A simple [Fragment] subclass.
@@ -20,8 +24,22 @@ class StarsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stars, container, false)
+        val rootView = inflater.inflate(com.example.bottomnavbar.R.layout.fragment_stars, container, false)
+
+        val rotatingImageView: ImageView = rootView.findViewById(com.example.bottomnavbar.R.id.rotatingImageView)
+
+        // Start the rotation animation
+        startRotationAnimation(rotatingImageView)
+
+        return rootView
+
     }
 
-
+    fun startRotationAnimation(view: View) {
+        val rotateAnimation = AnimatorInflater.loadAnimator(
+            view.context, com.example.bottomnavbar.R.animator.rotate_animation
+        ) as ObjectAnimator
+        rotateAnimation.target = view
+        rotateAnimation.start()
+    }
 }
