223c8ffbb2f1d49edcaa8e8fda9d3ef52b740d9a
ELOUAIZI Ayoub <ayoubelouaizi@gmail.com>
2023-12-13 09:23:13 +0100

    add dependencies for the fire-auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,19 @@ dependencies {
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.4")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.0")
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
 }
